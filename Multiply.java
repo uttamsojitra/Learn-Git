009b9a6ab112b13e6fd30c43031affbd43f027d4
Uttam Sojitra <uttamsojitra635@gmail.com>
2023-01-24 16:17:23 +0530

    multiplication

---
+++ Multiply.java
@@ -0,0 +1,10 @@
+public class Multiply 
+{  
+    public static void main(String args[])   
+    {  
+    int n1 = 225, n2 = 115, mul;  
+    mul = n1 * n2;  
+    System.out.println("The mul is: "+mul);  
+    }  
+}  
+    
