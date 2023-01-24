dff52e6683bac0e8d0b509a235b3beedd05fd370
Uttam Sojitra <uttamsojitra635@gmail.com>
2023-01-24 17:13:17 +0530

    added implementation

---
+++ Diff.java
@@ -0,0 +1,9 @@
+public class Diff {
+    public static void main(String args[])   
+{  
+int n1 = 225, n2 = 115, diff;  
+diff = n1 + n2;  
+System.out.println("The diff  is: "+diff);  
+}  
+    
+}
